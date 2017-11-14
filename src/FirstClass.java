6064918c06f03820e8a0d541ec45f947623eb4ee
Nakachi Ren <e175742@ie.u-ryukyu.ac.jp>
2017-11-14 15:03:51 +0900

    last commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,11 +1,11 @@
 public class FirstClass {
     public static void main(String[] args) {
-    try{
-        String str = null;
-        str.length();
-    }catch (NullPointerException e){
-        System.out.println(e + "が発生");
-        System.out.println(e.getMessage());
-    }
+        try{
+            String str = "3．14";
+            double value = Double.parseDouble(str);
+        }catch (NullPointerException e){
+            System.out.println(e + "が発生");
+            System.out.println(e.getMessage());
+        }
     }
 }

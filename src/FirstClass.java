0379e4860ecf9b70576f38f71021d864f308cd56
Nakachi Ren <e175742@ie.u-ryukyu.ac.jp>
2017-11-14 11:36:24 +0900

    first commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
-    public static void main(String[] args){
-        String str = null;
-        str.length();
+    public static void main(String[] args) {
+    String str = null;
+    str.length();
     }
 }
